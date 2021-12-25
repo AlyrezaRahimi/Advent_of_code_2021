09e3033060f2b0d0041a790656b6f7b1fc95b5a6
Alireza Rahimi <94568779+AlyrezaRahimi@users.noreply.github.com>
2021-12-25 14:05:11 +0330

    Add files via upload

---
+++ Day 1/Day_1.py
@@ -0,0 +1,8 @@
+
+input_data = []
+
+with open("F:\Work Base\Python\Advent of Code 2021\Day 1\Data.txt") as Data:
+    for i in Data:
+        input_data.append(i.rstrip())
+        
+print(input_data)
