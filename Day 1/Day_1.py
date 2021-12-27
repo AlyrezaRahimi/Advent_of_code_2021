7771867c5d408a8d5c643ed2d04b72fcd9df5a16
Sino <alsinoreza@gmail.com>
2021-12-27 22:49:37 +0330

    i finished puzzle of day one

--- Day 1/Day_1.py
+++ Day 1/Day_1.py
@@ -1,8 +1,19 @@
 
 input_data = []
 
-with open("F:\Work Base\Python\Advent of Code 2021\Day 1\Data.txt") as Data:
+with open("F:\Work Base\Python\Advent_of_code_2021\Day 1\Data.txt") as Data:
     for i in Data:
-        input_data.append(i.rstrip())
+        input_data.append(i)
         
-print(input_data)
+counter_change = 0
+input_data_num = int(input_data[0])
+
+for i in range(1,len(input_data)):
+    if int(input_data[i]) > input_data_num:
+        input_data_num = int(input_data[i])
+        counter_change += 1
+        print(input_data[i])
+    else:
+        input_data_num = int(input_data[i])
+
+print("number of change's",counter_change)
