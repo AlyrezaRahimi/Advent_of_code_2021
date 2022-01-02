82de362f6d44650fbbed74ed6ae8b8fb9c1cd0e1
Sino <alsinoreza@gmail.com>
2022-01-02 14:03:16 +0330

    finished phase one of puzzle we must verify this solve

--- Day 2/Day_2.py
+++ Day 2/Day_2.py
@@ -4,3 +4,20 @@ with open("F:\Work Base\Python\Advent_of_code_2021\Day 2\input.txt") as Data_2:
     for i in Data_2:
         input_data.append(i.strip())
         
+H = 0
+W = 0
+#forward X
+#up X
+#down X  
+        
+for i in range(0,len(input_data)):
+    word = input_data[i]
+    if word[0] == "f":
+        H = H + int(word[8])
+    elif word[0] == "u":
+        W = W - int(word[3])
+    elif word[0] == "d":
+        W = W + int(word[5])
+        
+
+print("position X:",H,"---","position Y:",W)
