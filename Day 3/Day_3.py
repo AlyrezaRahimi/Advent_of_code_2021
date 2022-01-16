02440256e3d0f907d525d33d7d0b0d13758ce96d
AlirezaRahimi <alirezarahimi1380@yahoo.com>
2022-01-16 16:08:53 +0330

    start of day 3

---
+++ Day 3/Day_3.py
@@ -0,0 +1,7 @@
+bin_input = []
+
+with open("F:\Work Base\Python\Advent_of_code_2021\Day 3\Bin input's.txt") as Data3:
+    for i in Data3:
+        bin_input.append(i.strip())
+        
+print(bin_input)
