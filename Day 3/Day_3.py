adabbb8ec5afaf7f057b9f3f15683c26d38d47e2
AlirezaRahimi <alirezarahimi1380@yahoo.com>
2022-01-16 16:35:27 +0330

    i find solution now i know how to solve this puzzle

--- Day 3/Day_3.py
+++ Day 3/Day_3.py
@@ -1,7 +1,18 @@
 bin_input = []
 
-with open("F:\Work Base\Python\Advent_of_code_2021\Day 3\Binary input's.txt") as Data3:
+with open("F:\Work Base\Python\Advent_of_code_2021\Day 3\Test.txt") as Data3:
     for i in Data3:
         bin_input.append(i.strip())
-        
-print(bin_input)
+      
+epsilon = ""
+gamma = ""
+Bin_data = 0
+
+num = 0  
+while num < 1:
+    for i in range(0,len(bin_input)):
+        Bin_data = bin_input[i]
+        print(Bin_data[num])
+    
+    
+    num += 1
