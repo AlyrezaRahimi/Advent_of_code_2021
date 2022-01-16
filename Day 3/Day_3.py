451e1764d203bedba01dee062051834fd1d66ef0
AlirezaRahimi <alirezarahimi1380@yahoo.com>
2022-01-16 16:12:37 +0330

    fix the input data on git

--- Day 3/Day_3.py
+++ Day 3/Day_3.py
@@ -1,6 +1,6 @@
 bin_input = []
 
-with open("F:\Work Base\Python\Advent_of_code_2021\Day 3\Bin input's.txt") as Data3:
+with open("F:\Work Base\Python\Advent_of_code_2021\Day 3\Binary input's.txt") as Data3:
     for i in Data3:
         bin_input.append(i.strip())
         
