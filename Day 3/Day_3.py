58886b58c4b0fa9227749f14a27e6df95a6af0f0
AlirezaRahimi <alirezarahimi1380@yahoo.com>
2022-01-16 16:53:25 +0330

    i solved the puzzle now i must convert bin number to flout and * them

--- Day 3/Day_3.py
+++ Day 3/Day_3.py
@@ -7,12 +7,34 @@ with open("F:\Work Base\Python\Advent_of_code_2021\Day 3\Test.txt") as Data3:
 epsilon = ""
 gamma = ""
 Bin_data = 0
+one = 0
+zero = 0
+
 
 num = 0  
-while num < 1:
+while num < len(bin_input[0]):
     for i in range(0,len(bin_input)):
+        
         Bin_data = bin_input[i]
-        print(Bin_data[num])
-    
+        if int(Bin_data[num]) == 0:
+            zero += 1
+        elif int(Bin_data[num]) == 1:
+            one += 1
+            
+    if zero > one:
+        gamma = gamma + "0"
+        epsilon = epsilon + "1"
+        
+        zero = 0
+        one = 0
+        
+    elif one > zero:
+        gamma = gamma + "1"
+        epsilon = epsilon + "0"
+        
+        zero = 0
+        one = 0
     
     num += 1
+
+print("gamma is: ",gamma,"\n","epsilon is: ",epsilon)
