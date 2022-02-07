4ee099a9a80fb6e0bcd34caa9091041410816d19
AlirezaRahimi <alirezarahimi1380@yahoo.com>
2022-02-07 18:58:48 +0330

    at this time code can get main number's for comparison and we can find how many matrix we have

--- Day 4/Day_4.py
+++ Day 4/Day_4.py
@@ -1,6 +1,3 @@
-from json.tool import main
-from os import remove
-
 
 test = []
 with open("F:\Work Base\Python\Advent_of_code_2021\Day 4\Test.txt") as Data:
@@ -9,8 +6,30 @@ with open("F:\Work Base\Python\Advent_of_code_2021\Day 4\Test.txt") as Data:
         
 #get number's
 main_num = test[0]
-for i in range(0,len(main_num)):
+cr_num = ""
+final_num = []
+i = 0
+
+while i < len(main_num):
     if main_num[i] == ",":
-        main_num = main_num.replace(",","\n")
+        pass
+    else:
+        while main_num[i] != ",":
+            cr_num = cr_num + main_num[i]
+            i += 1
+            if i == len(main_num):
+                break
+        final_num.append(cr_num)
+        cr_num = ""
+    i += 1
         
-        
+print("main number's is: ",final_num)
+
+#get number's of matrix for loop break point
+number_of_matrix = 0
+
+for i in range(0,len(test)):
+    if test[i] == "":
+        number_of_matrix += 1
+
+print("number's of matrix is: ",number_of_matrix)
