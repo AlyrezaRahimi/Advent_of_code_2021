7672c90c62141104906635137448d4edfe63b7cc
AlirezaRahimi <alirezarahimi1380@yahoo.com>
2022-07-31 13:45:36 +0430

    code succsessfully removed those line's are not Horizental or Vertical

--- Day 5/Day_5.py
+++ Day 5/Day_5.py
@@ -5,7 +5,7 @@ points_of_lines = []
 
 with open ("F:\Work Base\Python\Advent_of_code_2021\Day 5\line_x,y.txt", "r") as Lines:
     for point in Lines:
-        points_of_lines.append(point.strip())
+        points_of_lines.append(point)
 
 
 #remove '->' and ','
@@ -31,5 +31,44 @@ while flag < len(points_of_lines):
     points_of_lines.remove(points_of_lines[first_in_the_list])
     points_of_lines.append(group_of_points)
     flag += 1
+
+#print(points_of_lines)
+
+#remove not same x1,x2 or y1,y2
+
+
+def remove_unuse_points(line):
+    if int(chart_x1) != int(chart_x2) and int(chart_y1) != int(chart_y2):
+        points_of_lines.remove(line)
+    else:
+       global lineControler
+       lineControler += 1
+
+lineControler = 0
+for lines in range(0,len(points_of_lines)):
     
-print(points_of_lines)
+    line = points_of_lines[lineControler]
+    chart_x1 = ""
+    chart_x2 = ""
+    chart_y1 = ""
+    chart_y2 = ""
+    counter = 0
+    
+    for point in line:
+        
+        if point == ",":
+            counter += 1
+        elif counter == 0:
+            chart_x1 += point
+        elif counter == 1:
+            chart_y1 += point
+        elif counter == 2:
+            chart_x2 += point
+        else:
+            chart_y2 += point
+            
+    remove_unuse_points(line)   
+
+with open("new_list.txt","w") as newList:
+    for line in points_of_lines:
+        newList.write(line)
